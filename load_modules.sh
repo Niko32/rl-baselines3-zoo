385bca4d3554494ee1df1fca56284849a582b822
Niko <be66akax@studserv.uni-leipzig.de>
2024-03-27 15:07:39 +0100

    Add files for running on HPC

---
+++ load_modules.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+module load PyTorch/1.12.1-foss-2022a-CUDA-11.7.0
+module load SWIG/4.0.2-GCCcore-11.3.0
---
+++ serial_slurm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --time=10:00:00
+#SBATCH --partition=paula
+
+python --version
+
